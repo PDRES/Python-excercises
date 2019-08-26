61878e3348a68aaba6b36046ac936d3e24d2c0fb
PDRES <47571835+PDRES@users.noreply.github.com>
2019-08-26 10:55:00 +0200

    Add files via upload

---
+++ How to create packages/script.py
@@ -0,0 +1,6 @@
+from paquete.action.alerta import* 
+from paquete.execution.execution import execute
+from paquete.execution.execution import execution
+alert()
+execute()
+execution()
---
+++ How to create packages/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name="paquete",
+	version="0.1",
+	decription="Ejemplo",
+	Author="PDRES", 
+	email="none", 
+	Web="In progress",
+	sripts=[],
+	packages=["paquete","paquete.execution","paquete.action"] 
+	)
